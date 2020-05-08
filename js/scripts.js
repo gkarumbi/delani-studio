1a97f4808c26230c4f5c3cb8c43410a0eff35de2
gkarumbi <gkarumbi@gmail.com>
2020-05-08 17:56:09 +0300

    1st push to GH

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,25 @@ $(document).ready(function(event){
     $(".product-icon").click(function(){
         //alert("product works!");
         $(".show-product-management").toggleClass("hide-info");
+        
     });
+
+   
+    
 });
+
+//Adding Mouse  hover function
+// $(document).ready(function(event){
+
+//     // $(".img-fluid").on("mouseenter","img",function(){
+//     //     //$(this).find(".overlay").fadeIn(400);
+//     //     alert("image hover");
+//     // });
+//     // $(".portfolio").on("mouseleave","img",function(){
+//     //     $(this).find(".overlay").stop().fadeOut(400);
+//     // });
+//     $(".portfolio").on("mouseenter", function(){
+//         $(this).find("img").css({"border": "2px solid green"}).fadeIn(300);
+//     });
+// });
+
